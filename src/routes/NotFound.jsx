1e8d7c38bb2cb50bfb05f0b47a969d16845599e3
TheBao07102003 <thebao71003@gmail.com>
2024-04-27 20:09:10 +0700

    LogIn button

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,10 +1,12 @@
+import LogIn from '../components/LogIn/LogIn'
 import Menu from '../components/Menu'
 
 function NotFound() {
 	return (
 		<div className="flex h-full flex-grow flex-col items-center justify-center">
-			<h2 className="text-3xl font-bold">Oops! 404</h2>
-			<h1 className="text-5xl text-gray-600">Page not found</h1>
+			<LogIn/>
+			{/* <h2 className="text-3xl font-bold">Oops! 404</h2>
+			<h1 className="text-5xl text-gray-600">Page not found</h1> */}
 		</div>
 	)
 }
