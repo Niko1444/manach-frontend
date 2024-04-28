1ea28139d644e31d63c094dfeeb230a0608f8e21
TheBao07102003 <thebao71003@gmail.com>
2024-04-28 13:56:52 +0700

    Cart Btn Component

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,12 +1,17 @@
+import CartBtn from '../components/Cart btn/CartBtn'
 import LogIn from '../components/LogIn/LogIn'
 import Menu from '../components/Menu'
 import SignUp from '../components/SignUp/SignUp'
 
+
+
+
 function NotFound() {
 	return (
 		<div className="flex h-full flex-grow flex-col items-center justify-center">
-			<LogIn/>
-			<SignUp/>
+			
+		
+			
 			<h2 className="text-3xl font-bold">Oops! 404</h2>
 			<h1 className="text-5xl text-gray-600">Page not found</h1>
 		</div>
