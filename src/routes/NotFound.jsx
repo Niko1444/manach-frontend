2f4bd4bda285278b794b87e18ce71d260355f332
phuongnam1556 <trandangphuongnam2003@gmail.com>
2024-05-08 14:02:52 +0700

    pull

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,13 +1,15 @@
 
 import Menu from '../components/Menu'
 import Sort from '../components/Sort/Sort'
-import Create from '../components/Create/Create'
+// import Create from '../components/Create/Create'
+import AddToCart from '../components/AddToCart/AddToCart'
 
 function NotFound() {
 	return (
 		<div className="flex h-full flex-grow flex-col items-center justify-center">
 			<Sort/>
-			<Create/>
+			{/* <Create/> */}
+			<AddToCart/>
 			<h2 className="text-3xl font-bold">Oops! 404</h2>
 			<h1 className="text-5xl text-gray-600">Page not found</h1>
 		</div>
