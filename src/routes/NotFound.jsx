137e77bbe9afa2692b420740135f62c48b2397bf
TheBao07102003 <thebao71003@gmail.com>
2024-05-10 14:06:41 +0700

    fix AtManach

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,9 +1,9 @@
-import AtManach from "../components/AtManach/AtManach"
+
 
 function NotFound() {
 	return (
 		<div className="flex h-full flex-grow flex-col items-center justify-center">
-			<AtManach/>
+			
 		</div>
 	)
 }
