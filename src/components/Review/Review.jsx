0dba99383bc0e3498ee9d523e9a9b96af3a4909e
hoang <imhoangnguyen0923@gmail.com>
2024-05-14 17:55:22 +0700

    review demo

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,86 +1,150 @@
 import React from 'react'
-import { useState } from 'react'
 
 const Review = () => {
-	const [state, setState] = useState({})
 	return (
-		<div
-			className="w-[733px] h-[313px] relative"
-			onClick={() => {
-				setState({ boxShadow: '10px 10px 0px 0px rgba(0,0,0,0.25) inset' })
-
-				// After 1 second, reset the box shadow to none
-				setTimeout(() => {
-					setState({ boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' })
-				}, 200)
-			}}
-		>
+		<div>
 			<button
-				className="absolute h-full w-full rounded-lg bg-none hover:shadow-[10px_10px_0px_0px_rgba(0,0,0,0.25)]"
-				style={state}
+				style={{
+					width: '733px',
+					height: '313px',
+					display: 'grid',
+					gridTemplateColumns: '235px 1fr', // Ensure image and text are in two columns
+					alignItems: 'center',
+					background: '#FFF',
+					boxShadow: '0px 15px 40px 0px #C2C9AC',
+					cursor: 'pointer',
+					padding: '25px',
+				}}
 			>
-				{/* content */}
-				<div className="left-0 top-0 h-full w-full rounded-sm">
-					{/* content  */}
-					<div className="mt-8 h-full w-full">
-						{/* text */}
-						<div
-							className="mb-[0.87rem]"
-							style={{
-								color: '#485935',
-                                fontFamily: 'Poppins',
-                                fontSize: '22px',
-                                fontStyle: 'normal',
-                                fontWeight: '500',
-                                lineHeight: 'normal',
-							}}
-						>
-							Explore popular fruits
-						</div>
+				{/* Image Column */}
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<img
+						src="/src/assets/banana.png"
+						alt="Cavendish Banana"
+						style={{
+							width: '235px',
+							height: '233px',
+						}}
+					/>
+				</div>
 
-						{/* image  */}
-						<div>
-							<div className="ml-auto h-fit w-full">
-							<img
-								src="/src/assets/banana.png"
-								alt=""
+				{/* Text Column */}
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						paddingLeft: '62px',
+						paddingRight: '28px',
+					}}
+				>
+					<div
+						style={{
+							color: '#485935',
+							fontFamily: 'Poppins',
+							fontSize: '22px',
+							fontWeight: '500',
+						}}
+					>
+						Cavendish Banana
+					</div>
+					<div className="flex items-center" style={{ marginBottom: '19px' }}>
+						<div className="flex align-middle">
+							<i
+								class="fa fa-star"
 								style={{
-									background: 'url(<path-to-image>) lightgray 50% / cover no-repeat',
-                                    boxShadow: '0px 6px 4px 0px rgba(0, 0, 0, 0.25)',
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
 								}}
-								className="mx-auto h-[7.0625rem] w-[10.5625rem]"
-								/>
-							</div>
-						</div>
-
-						<div className="mx-auto my-4 h-[0.0625rem] w-[13.3125rem] bg-green_dark1"></div>
-
-						<div className="mx-auto text-center">
-							<div className="flex items-center justify-center">
-								<span className="mr-1">See all</span>
-								<div className="flex align-middle">
-									<div className="my-auto h-[0.125rem] w-[1.875rem] bg-green_dark1"></div>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										width="7"
-										height="12"
-										viewBox="0 0 7 12"
-										fill="none"
-										className="-mx-1.5"
-									>
-										<path
-											d="M1.49993 0.999956L5 6L1.50001 11"
-											stroke="#485935"
-											strokeWidth="2"
-										/>
-									</svg>
-								</div>
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '18px',
+								}}
+							></i>
+							<div
+								style={{
+									color: '#485935',
+									fontFamily: 'Poppins',
+									fontSize: '18px',
+									fontWeight: '300',
+								}}
+							>
+								(2400+ Reviews)
 							</div>
 						</div>
 					</div>
+					<div
+						style={{
+							color: '#000',
+							textAlign: 'justify',
+							fontFamily: 'Poppins',
+							fontSize: '16px',
+							fontWeight: '300',
+							marginBottom: '21px',
+						}}
+					>
+						Cavendish bananas are the fruits of one of a number of banana
+						cultivars belonging to the Cavendish subgroup of the AAA banana
+						cultivar group.
+					</div>
+					<div className="flex items-center">
+						<div
+							style={{
+								color: '#485935',
+								fontFamily: 'Poppins',
+								fontSize: '18px',
+								fontWeight: '700',
+								marginRight: '19px',
+							}}
+						>
+							Price:
+						</div>
+						<div
+							style={{
+								color: '#485935',
+								fontFamily: 'Poppins',
+								fontSize: '18px',
+								fontWeight: '700',
+							}}
+						>
+							$5/kg
+						</div>
+					</div>
 				</div>
-
-				{/* content */}
 			</button>
 		</div>
 	)
