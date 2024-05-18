fd696fb7060900241839ed1975b724fd4adb913d
TheBao07102003 <thebao71003@gmail.com>
2024-05-18 12:46:59 +0700

    Create ProfileSetiing and Fill to Change..

--- src/routes/Home/Customer/Order/CusOrder.jsx
+++ src/routes/Home/Customer/Order/CusOrder.jsx
@@ -1,12 +1,12 @@
 import ChangeAddress from "../Setting.jsx/FillToChange/ChangeAddress"
 import ChangeEmail from "../Setting.jsx/FillToChange/ChangeEmail"
 import ChangePayment from "../Setting.jsx/FillToChange/ChangePayment"
-import ChangePhoneNum from "../Setting.jsx/FillToChange/ChangePhoneNum"
 
 const CusOrder = () => {
 	return <div>
+		
 		<ChangeAddress/>
-	</div>
+		</div>
 }
 
 export default CusOrder
