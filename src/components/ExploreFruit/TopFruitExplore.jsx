b9dbdf838125cec3295675af9bb23dd47b027981
hoang <imhoangnguyen0923@gmail.com>
2024-05-24 17:38:20 +0700

    Customer Store: update shadow when click into explore bar

--- src/components/ExploreFruit/TopFruitExplore.jsx
+++ src/components/ExploreFruit/TopFruitExplore.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const TopFruitExplore = ({ onClose }) => {
 	return (
 		
-		<div>
+		<div style={{
+			position: 'fixed',
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			zIndex: 1000, 
+		}}>
 			<div
 				style={{
 					width: '600px',
