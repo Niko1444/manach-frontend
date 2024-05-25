dd9edbe8c7037f49a26dfa780aaecb3d68e141f2
hoang <imhoangnguyen0923@gmail.com>
2024-05-25 22:14:05 +0700

    Fetch api for Re-order and Order history for Warehouse

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,273 +1,288 @@
 import React from 'react'
 import banana from '/D/IU/SE/Project/manach-frontend/src/assets/banana.png'
 import AddToCart from '../AddToCart/AddToCart.jsx'
-import BuyNow from '../BuyNow/BuyNow.jsx'
 import Exit from '../Exit/Exit.jsx'
 
-const Detail = () => {
+const Detail = ({ onClose }) => {
 	return (
-		<div
-			style={{
-				width: '1097px',
-				height: '692px',
-				display: 'grid',
-				gridTemplateColumns: '889px 1fr',
-				opacity: '0.8',
-				background: 'var(--Green-light0, rgba(137, 185, 0, 0.50))',
-			}}
+		<div style={{
+			position: 'fixed',
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			zIndex: 1000, 
+		}}
 		>
 			<div
-				className="my-auto ml-[109px]"
 				style={{
-					width: '733px',
-					height: '520px',
+					width: '1097px',
+					height: '692px',
 					display: 'grid',
-					gridTemplateColumns: '1fr 415px',
+					gridTemplateColumns: '889px 1fr',
 					borderRadius: '10px',
-					background: '#FFF',
+					background: '#89B90080',
 					boxShadow: '0px 15px 40px 0px #C2C9AC',
 				}}
 			>
-				<div className="ml-[20px] mt-[21px]">
-					<div
-						style={{
-							width: '298px',
-							height: '259px',
-							border: '1px solid #485935',
-						}}
-					>
-						<img src={banana} alt="" />
-					</div>
-					<div
-						className="mt-[25px]"
-						style={{ color: '#485935', fontSize: '18px', fontWeight: '600' }}
-					>
-						Description
-					</div>
-					<div
-						className="h-fixed w-[275px]"
-						style={{
-							color: '#000',
-							fontSize: '16px',
-							fontWeight: '300',
-							textAlign: 'justify',
-						}}
-					>
-						Apple banana, also known as Candy Apple Banana, is a distinct
-						variety native to Hawaii's tropical rainforest region. Sweet, firm,
-						and pink, they're perfect for snacking and desserts.
-					</div>
-				</div>
-
-				<div className="ml-[46px] mt-[42px]">
-					<div
-						className="w-fixed mb-[28px] h-[33px]"
-						style={{ color: '#93A267', fontSize: '22px', fontWeight: '600' }}
-					>
-						Apple Banana
-					</div>
-					<div
-						className="mb-[28px] flex"
-						style={{
-							color: '#000',
-							fontSize: '16px',
-							fontWeight: '300',
-							textAlign: 'justify',
-						}}
-					>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '18px',
-							}}
-						></i>
+				<div
+					className="my-auto ml-[109px]"
+					style={{
+						width: '733px',
+						height: '520px',
+						display: 'grid',
+						gridTemplateColumns: '1fr 415px',
+						borderRadius: '10px',
+						background: '#FFF',
+						boxShadow: '0px 15px 40px 0px #C2C9AC',
+					}}
+				>
+					<div className="ml-[20px] mt-[21px]">
 						<div
-							style={{ color: '#485935', fontSize: '18px', fontWeight: '300' }}
+							style={{
+								width: '298px',
+								height: '259px',
+								border: '1px solid #485935',
+							}}
 						>
-							(2400+ Reviews)
+							<img className='m-auto' src={banana} alt="" />
+						</div>
+						<div
+							className="mt-[25px]"
+							style={{ color: '#485935', fontSize: '18px', fontWeight: '600' }}
+						>
+							Description
+						</div>
+						<div
+							className="h-fixed w-[275px]"
+							style={{
+								color: '#000',
+								fontSize: '16px',
+								fontWeight: '300',
+								textAlign: 'justify',
+							}}
+						>
+							Apple banana, also known as Candy Apple Banana, is a distinct
+							variety native to Hawaii's tropical rainforest region. Sweet, firm,
+							and pink, they're perfect for snacking and desserts.
 						</div>
 					</div>
-					<div
-						style={{
-							width: '328px',
-							height: '321px',
-							border: '1px solid #485935',
-						}}
-					>
-						<div className="ml-[47px] mt-[31px]">
-							<div className="mb-[20px] flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									Quantity:
-								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									10kg
-								</div>
-							</div>
 
-							<div className="mb-[20px] flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									Product ID:
-								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									B012344
-								</div>
+					<div className="ml-[46px] mt-[42px]">
+						<div
+							className="w-fixed mb-[28px] h-[33px]"
+							style={{ color: '#93A267', fontSize: '22px', fontWeight: '600' }}
+						>
+							Apple Banana
+						</div>
+						<div
+							className="mb-[28px] flex"
+							style={{
+								color: '#000',
+								fontSize: '16px',
+								fontWeight: '300',
+								textAlign: 'justify',
+							}}
+						>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '18px',
+								}}
+							></i>
+							<div
+								style={{ color: '#485935', fontSize: '18px', fontWeight: '300' }}
+							>
+								(2400+ Reviews)
 							</div>
+						</div>
+						<div
+							style={{
+								width: '328px',
+								height: '321px',
+								border: '1px solid #485935',
+							}}
+						>
+							<div className="ml-[47px] mt-[31px]">
+								<div className="mb-[20px] flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										Quantity:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										10kg
+									</div>
+								</div>
 
-							<div className="mb-[20px] flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									On-shelf date:
+								<div className="mb-[20px] flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										Product ID:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										B012344
+									</div>
 								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									03/01/2024
-								</div>
-							</div>
 
-							<div className="mb-[20px] flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									Supplier:
+								<div className="mb-[20px] flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										On-shelf date:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										03/01/2024
+									</div>
 								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									FreshBox
-								</div>
-							</div>
 
-							<div className="mb-[20px] flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									Price:
+								<div className="mb-[20px] flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										Supplier:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										FreshBox
+									</div>
 								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									$5/kg
-								</div>
-							</div>
 
-							<div className="flex">
-								<div
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '600',
-									}}
-								>
-									Ripeness:
+								<div className="mb-[20px] flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										Price:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										$5/kg
+									</div>
 								</div>
-								<div
-									className="ml-1.5"
-									style={{
-										color: '#485935',
-										fontSize: '18px',
-										fontWeight: '400',
-									}}
-								>
-									Unripe
+
+								<div className="flex">
+									<div
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '600',
+										}}
+									>
+										Ripeness:
+									</div>
+									<div
+										className="ml-1.5"
+										style={{
+											color: '#485935',
+											fontSize: '18px',
+											fontWeight: '400',
+										}}
+									>
+										Unripe
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div className="column-3 my-auto grid gap-[35px]">
-				<AddToCart />
-				<BuyNow />
-				<Exit />
+				<div className="column-2 my-auto grid gap-[35px]">
+					<AddToCart />
+					<div onClick={onClose}>
+						<Exit />
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,118 +1,156 @@
-import React from 'react'
+import { useState } from 'react'
 import banana from '../../assets/banana.png'
 import Ripe from '../StateRipe/Ripe.jsx'
 import Firm from '../StateRipe/Firm.jsx'
 import UnRipe from '../StateRipe/UnRipe.jsx'
+import Detail from '../Detail/Detail.jsx'
 
 const Review = () => {
+	const [state, setState] = useState({});
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleButtonClick = () => {
+		setState({ boxShadow: '10px 10px 0px 0px rgba(0,0,0,0.25) inset' });
+		setIsModalOpen(true);
+
+		// After 200ms, reset the box shadow to none
+		setTimeout(() => {
+			setState({ boxShadow: '' });
+		}, 200);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
-		<div className='mb-[20px]'>
-			<div
-				style={{
-					width: '733px',
-					height: '313px',
-					display: 'grid',
-					gridTemplateColumns: '326px 1fr',
-					background: '#FFF',
-					borderRadius: '10px',
-					boxShadow: '0px 15px 40px 0px #C2C9AC',
-				}}
+		<div className={isModalOpen ? 'modal-open' : ''}>
+			<div 
+				className='mb-[20px]'
+				onClick={handleButtonClick}
 			>
-				<div className="m-auto">
-					<img src={banana} alt="" />
-				</div>
-				<div className="mt-[56px]">
-					<div style={{ color: '#485935', fontSize: '22px', fontWeight: '500' }}>Cavendish Banana</div>
-					<div className="mb-[19px] flex">
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '10px',
-							}}
-						></i>
-						<i
-							class="fa fa-star"
-							style={{
-								color: '#CADBB7',
-								fontSize: '1.25rem',
-								marginRight: '18px',
-							}}
-						></i>
+				<div
+					style={{
+						width: '733px',
+						height: '313px',
+						display: 'grid',
+						gridTemplateColumns: '326px 1fr',
+						background: '#FFF',
+						borderRadius: '10px',
+						boxShadow: '0px 15px 40px 0px #C2C9AC',
+					}}
+				>
+					<div className="m-auto">
+						<img src={banana} alt="" />
+					</div>
+					<div className="mt-[56px]">
+						<div style={{ color: '#485935', fontSize: '22px', fontWeight: '500' }}>Cavendish Banana</div>
+						<div className="mb-[19px] flex">
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '10px',
+								}}
+							></i>
+							<i
+								class="fa fa-star"
+								style={{
+									color: '#CADBB7',
+									fontSize: '1.25rem',
+									marginRight: '18px',
+								}}
+							></i>
+							<div
+								style={{ color: '#485935', fontSize: '18px', fontWeight: '300' }}
+							>
+								(2400+ Reviews)
+							</div>
+						</div>
 						<div
-							style={{ color: '#485935', fontSize: '18px', fontWeight: '300' }}
+							className="h-fixed w-[379px]"
+							style={{ color: '#000', fontSize: '16px', fontWeight: '300' }}
 						>
-							(2400+ Reviews)
+							Cavendish bananas are the fruits of one of a number of banana
+							cultivars belonging to the Cavendish subgroup of the AAA banana
+							cultivar group.
 						</div>
-					</div>
-					<div
-						className="h-fixed w-[379px]"
-						style={{ color: '#000', fontSize: '16px', fontWeight: '300' }}
-					>
-						Cavendish bananas are the fruits of one of a number of banana
-						cultivars belonging to the Cavendish subgroup of the AAA banana
-						cultivar group.
-					</div>
-					<div className="flex">
-						<div className="my-auto flex">
-							<div
-								className="mr-[19px]"
-								style={{
-									color: '#485935',
-									fontSize: '18px',
-									fontWeight: '700',
-								}}
-							>
-								Price:
+						<div className="flex">
+							<div className="my-auto flex">
+								<div
+									className="mr-[19px]"
+									style={{
+										color: '#485935',
+										fontSize: '18px',
+										fontWeight: '700',
+									}}
+								>
+									Price:
+								</div>
+								<div
+									className="mr-[28px]"
+									style={{
+										color: '#485935',
+										fontSize: '18px',
+										fontWeight: '700',
+									}}
+								>
+									$5/kg
+								</div>
 							</div>
-							<div
-								className="mr-[28px]"
-								style={{
-									color: '#485935',
-									fontSize: '18px',
-									fontWeight: '700',
-								}}
-							>
-								$5/kg
+							<div className="my-auto flex">
+								<div className="mr-[10px]">
+									<Ripe />
+								</div>
+								<div className="mr-[10px]">
+									<Firm />
+								</div>
+								<UnRipe />
 							</div>
-						</div>
-						<div className="my-auto flex">
-							<div className="mr-[10px]">
-								<Ripe />
-							</div>
-							<div className="mr-[10px]">
-								<Firm />
-							</div>
-							<UnRipe />
 						</div>
 					</div>
 				</div>
 			</div>
+
+			{/* Modal */}
+			{isModalOpen && (
+				<div className="fixed inset-0 z-50 flex items-center justify-center">
+					{/* Modal Backdrop */}
+					<div className="fixed inset-0 bg-black opacity-50"></div>
+
+					{/* Modal Content */}
+					<div className="relative bg-white p-4 rounded-lg z-10 shadow-lg">
+						<div>
+							<Detail onClose={closeModal} />
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
