2ec779fb11bda29b766a0f3ed7eb6c99b0f85b3e
TheBao07102003 <thebao71003@gmail.com>
2024-05-25 21:50:31 +0700

    fetch API for Inventory

--- src/routes/Home/Admin/Warehouse/Inventory/TableDecreasingSorted.jsx
+++ src/routes/Home/Admin/Warehouse/Inventory/TableDecreasingSorted.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const TableDecreasingSorted = () => {
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await fetch('http://localhost:8080/warehouse')
+				const result = await response.json()
+				// Sort products by total quantity in descending order
+				const sortedProducts = result.content.getProducts
+					.slice()
+					.sort((a, b) => {
+						const totalQuantityA = calculateTotalQuantity(
+							a.warehouse_products,
+							a.shelf_products,
+						)
+						const totalQuantityB = calculateTotalQuantity(
+							b.warehouse_products,
+							b.shelf_products,
+						)
+						return totalQuantityB - totalQuantityA // Sort in decreasing order
+					})
+				setProducts(sortedProducts)
+			} catch (error) {
+				console.error('Error fetching products:', error)
+			}
+		}
+
+		fetchProducts()
+	}, [])
+
+	const convertGoogleDriveLink = (url) => {
+		if (!url) {
+			return ''
+		}
+
+		// Check if the URL is a direct link to the image file
+		if (url.startsWith('https://drive.google.com/uc?export=view&id=')) {
+			return url
+		}
+
+		// Extract the file ID from the Google Drive link
+		const fileIdMatch = url.match(/\/file\/d\/([a-zA-Z0-9_-]+)/)
+		if (fileIdMatch) {
+			// Construct a direct image URL using the file ID
+			return `https://drive.google.com/uc?export=view&id=${fileIdMatch[1]}`
+		}
+
+		return url
+	}
+
+	const calculateTotalQuantity = (warehouseProducts, shelfProducts) => {
+		const warehouseQuantity = warehouseProducts.reduce(
+			(acc, item) => acc + item.quantity,
+			0,
+		)
+		const shelfQuantity = shelfProducts.reduce(
+			(acc, item) => acc + item.quantity,
+			0,
+		)
+		return warehouseQuantity + shelfQuantity
+	}
+
+	const determineStatus = (quantity) => {
+		if (quantity === 0) {
+			return { status: 'Out of stock', color: '#F07167' }
+		} else if (quantity < 6) {
+			return { status: 'Low stock', color: '#FFD600' }
+		} else if (quantity < 11) {
+			return { status: 'Near-low stock', color: '#A0D900' }
+		} else {
+			return { status: 'High stock', color: '#485935' }
+		}
+	}
+
 	return (
 		<div style={{ padding: '1rem' }}>
-			<table style={{ minWidth: '100%', borderCollapse: 'collapse', border: '1px solid #485935', }}>
+			<table
+				style={{
+					minWidth: '100%',
+					borderCollapse: 'collapse',
+					border: '1px solid #485935',
+				}}
+			>
 				<thead>
 					<tr>
 						<th
@@ -60,218 +140,75 @@ const TableDecreasingSorted = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/4909/5b01/5c7b2c14b10c19d782f56e4027b91331?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TWUq4pBd0aAL-C3acl8AWSrQtgJlWvYZCKQF-YlFlr5eppudBKIR8lMMonNdGQMjN~IuoIx77naXHRNAXbS0qsS8-pei4Gq~qfY7141rDDDY~Fj0AiNrs~UORoxDL1yIbmnaur7~gfxB30m1dMxjsloI8~SEP0Cm781~aN4UDGN2AZjZ73C~mM87WcC~Qnx27nVfb0Tzn6evUWxHqYysN6eGmOuUePSwBA7dqRXsjmBvdx7qTN8aZERCyqjHB9eTh3zhS~YBCvuRV~QL4ubrQmRN8mbwwWVsSdmBfs~XIBr3Aofyvf-HE4yGnQ26QprI5nlPbaP1HUYJtXfixuKFQQ__"style={{height:'70px'}} />
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Red Prawn Durian
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							15
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							High-stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/3535/736b/a39a05b881b5c81b05a85d4dbea84577?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vnksnha1KBkmIFIUAcydXzHyVxlc6ME8TM4f40L6o6IZbX9Athoduqd8ieaay9g71O-jj7bwxAlzL6qbYxb8SleCbVRi4FqeENP9BMycSbkgV~DCnAVOMOK1dqpTV8Kpbgz1ZVDCDGvAhz1BgLj-W2vXGqYyNfsiKHlgm8MFs-dRnCNlMl36Kn67lAmvnd-6DUiTjVb2tgAOmSJni58Tng1jhz4xv5buvADAk9ej86ErO2x42qlXp2ZdXsG8Y1HVIUGUAEkLaFSpxwOwip-DOk8QHgvKrlbOXe3KmrxO5ZWcuj-~aCNi3QMHcdktqScbjNEvmHto-8qtUJfSeaBQmw__" style={{height:'70px'}}/>
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Honey Mango
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							13
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							High-stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/5943/95c6/d36be54c185c6e25cea323dc3199d972?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jrvNQwcHNY39ZZv8ZJR9aDSyO3EMqN2mRJ08v82ZYYz8c397XwyJfH9g-yMZM1ILn5fPiFal8tl7ch9QeAz88bDZKVKQPFq2LicRDjEuIK203QiBcunW0rrW4lULqNNmZlu12k44Sn6Jioo8SvDCxDskl7QOxusC1Fn6hPSRdUm67jIM2PysHthfTERhCid49PYoe2pBziyoJvlFdT-Lr4VlKVjqUuDA3WyXexT854Eoi4X76L8NS6BRSDAYIz6Q6MA~CuSjUD6jz59Z1o3eaGCw13Tve4kmsYol7I133pSLpOm1u1OGRtQjGC0XCNGXjPwd~sZA2vpwWdCDboTJAg__" style={{height:'70px'}}/>
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Red Spanish Papaya
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							12
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							High-stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/9e14/590a/cb89f9dca79c3f3c95b32881f5b51694?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SVMbOlJgp60ixor48P0GqPMTtCx4UiyuyTfqSmQb1LVGvTgOuNqL1HabRTY2Rf5mTFkGq9bdR-jDPqNDJSGbLdVNeUksUJvIRQl82OABQ5-aj1aYsW73MXOpHCT84re1XvMlrbJh9-HG46Vxec~bZO8uyly3H6UC-odYViLv3RBxwp99pYJVowF3tCNCt2wtilTKMRDJzV18wE5w-TtGxNA6eh8LGZ8HZzOnEFPmSV~sxuguZjSfsr5Esj5oXPyO-T1pK~SI3j9NH2Xwjw2fAzIy9SHWRG-sZYWWxia8HI~qlBawKA3YMb8twvK3X0UVsNLurNzPXtBA3B3nfDNCMg__" style={{height:'70px'}}/>
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Sunrise Papaya
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							11
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							High-stock
-						</td>
-					</tr>
+					{products.map((product) => {
+						const totalQuantity = calculateTotalQuantity(
+							product.warehouse_products,
+							product.shelf_products,
+						)
+						const { status, color } = determineStatus(totalQuantity)
+						const imageUrl =
+							convertGoogleDriveLink(product.product_img) ||
+							'https://via.placeholder.com/70'
+						return (
+							<tr key={product.product_id}>
+								<td
+									style={{
+										padding: '8px',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									<img
+										src={imageUrl}
+										alt={product.product_name}
+										style={{ height: '70px' }}
+									/>
+								</td>
+								<td
+									style={{
+										color: '#485935',
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontWeight: '400',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									{product.product_name}
+								</td>
+								<td
+									style={{
+										color: '#485935',
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontWeight: '400',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									{totalQuantity}
+								</td>
+								<td
+									style={{
+										color: color,
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontStyle: 'italic',
+										fontWeight: '900',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+									}}
+								>
+									{status}
+								</td>
+							</tr>
+						)
+					})}
 				</tbody>
 			</table>
 		</div>
--- src/routes/Home/Admin/Warehouse/Inventory/TableProductList.jsx
+++ src/routes/Home/Admin/Warehouse/Inventory/TableProductList.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const baseURL = 'http://localhost:8080'
+export const BASE_URL_IMG = 'http://localhost:8080/public/imgs'
+
+export const http = axios.create({
+	baseURL: baseURL,
+})
+
+const defaultImageUrl =
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzAujBL24JokIK0OASxaL0VhvKjOIQjWeZ_aN8nDJYHw&s'
 
 const TableProductList = () => {
+	const [products, setProducts] = useState([])
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			try {
+				const response = await http.get('/warehouse')
+				const shuffledProducts = shuffleArray(response.data.content.getProducts)
+				setProducts(shuffledProducts)
+			} catch (error) {
+				console.error('Error fetching products:', error)
+			}
+		}
+
+		fetchProducts()
+	}, [])
+
+	const shuffleArray = (array) => {
+		let currentIndex = array.length,
+			temporaryValue,
+			randomIndex
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex -= 1
+			temporaryValue = array[currentIndex]
+			array[currentIndex] = array[randomIndex]
+			array[randomIndex] = temporaryValue
+		}
+		return array
+	}
+
+	const calculateTotalQuantity = (warehouseProducts, shelfProducts) => {
+		const warehouseQuantity = warehouseProducts.reduce(
+			(acc, item) => acc + item.quantity,
+			0,
+		)
+		const shelfQuantity = shelfProducts.reduce(
+			(acc, item) => acc + item.quantity,
+			0,
+		)
+		return warehouseQuantity + shelfQuantity
+	}
+
+	const determineStatus = (quantity) => {
+		if (quantity === 0) {
+			return { status: 'Out of stock', color: '#F07167' }
+		} else if (quantity < 6) {
+			return { status: 'Low stock', color: '#FFD600' }
+		} else if (quantity < 11) {
+			return { status: 'Near-low stock', color: '#A0D900' }
+		} else {
+			return { status: 'High stock', color: '#485935' }
+		}
+	}
+
 	return (
 		<div style={{ padding: '1rem' }}>
-			<table style={{ minWidth: '100%', borderCollapse: 'collapse', border: '1px solid #485935', }}>
+			<table
+				style={{
+					minWidth: '100%',
+					borderCollapse: 'collapse',
+					border: '1px solid #485935',
+				}}
+			>
 				<thead>
 					<tr>
 						<th
@@ -60,218 +131,72 @@ const TableProductList = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/c701/bbce/bc71e5f0a0e8828913b38cf375a3c5f9?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mBS5Bw9qH5-RHZDOh7YpolWRSHFjV-6rkFHtBY7NwJNDS7fWajAOE58Brs5BLZgcIyJNzlNK7-TZFcAiU0U3Jkp~ciJK2S1VAoAkb2UXNz0W~fsUrczaho1iAYrVtowFWKOlRKlUiQ-UTS-rdF5YNFdRujzf-UhJ90wMTauG6GKL-1nOiKxlNX489KIypxzZ9bZtjxLJKCKs019dPNT6LR4PpxTKzAog4bF3SmmeF6QnUlgkoCrD21qXswwEcjvzZSgKsyvnstKgkltyLN89fUv-zTI8jrLHMYEIgCsePHQD2Mb4k7ZfAO4kAaMxLB-~XIPKmoufbhkWlwPQXmAObg__"style={{height:'70px'}} />
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Strawberry Coconut
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							3
-						</td>
-						<td
-							style={{
-								color: '#FFD600',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							Low stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/aea6/9e6d/d66f9691e82cae98df7f353bc35373e9?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZXmuGoy9u2bD65mic3km--LhQ7Ac9S4NcVIwNbYhz2Yt6830gos-qwBo1pppZVmNBhtD9KSixVDiLl6dRTqHCouspH4e8LKFwHSWRPbxaOWUChEo5qa1cnfokvkdUWCtgyBszVK32BoKe3YmNFYtl~F~Aa5WGik3S4QdkqztRy2s2jh1sxo0zCpqR~zU-sBpOKRm2EGBUHUm1oLkMQcUw6DmW-yZqO1R20p4JmSJEEnsRGs~NNQp8rvA77DLietI175xViSaKt5SDcbUtaEJkLuJ4JGhYvnAti4x-y1WzRqwMVdfh~WuOszmqKw6w5hztFVGdYXCImt5mABD8TWlZw__"style={{height:'70px'}} />
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Cavendish Banana
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							2
-						</td>
-						<td
-							style={{
-								color: '#FFD600',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							Low stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/5a42/264f/cd89439c5d689fc2c6fea71734c58257?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EOx-DEZCEogXY64gCszfL-U14ljAYp3OzO~0MT5Eg~XYU1ehHUQL1BNZrC3WOSIZ-Iq~QkYgeG0dfHl0GvOATaFm~fGZKWNBgOV4A--qeRc3HGfB3~ki0BImKdMPn2FLwqq9SHfa1-ZDciEgZLdqHAuOrQ-bx-3RQW0yadmuO1qIWeJOcICHrcffRS90uoboHXqHGWtFyL5276IIJjDKqyrOikValFib--Riauzuv5GkHfigOUCH6x1~-yR0curMBQgu7BhqKT7b5r~ecVOO97QXbvluwkHs2LksZ5ifcxUmoqHyT4O0aIhflPR3QjKJfpqKJpGAdYBsnTGsOWJLNQ__"style={{height:'70px'}} />
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Vietnamese Durian
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							0
-						</td>
-						<td
-							style={{
-								color: '#F07167',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							Out of stock
-						</td>
-					</tr>
-					<tr>
-						<td
-							style={{
-								padding: '8px',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							<img src="https://s3-alpha-sig.figma.com/img/8a09/5151/d29bd1043ed49778b64ea24142504428?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JtadRkGvA5DN1PQ9kSRWmXh0T244neoML5yJb7~MhZrC6aFBid~PgJj2JXChS64oKXyrdkbQoEf8cakRMVhjrzX1AlEOyXbhcoOVNyXpZB8qm1w0hF5uKMB6IuXeeu1Bky8g-r7ulQuN3Ta0H4Ui99SYIB2nLGPBrBHmrG0CbYmWbog4ONKYF7dQsmi9CrAcgGN6Gtke0~5rF6ti5wCvdUym2yM31UhGd956H1nsEjBLu1h-oNjyGImHY1fBuYyC8ziVagyItg7VD8AsjwdxIfWWm9j3GoM-8qgpS6QqxXcQCzCcAKhaID5qFlSfvCoEN5sOb7ZfCQPH2d243Vuatg__"style={{height:'70px'}} />
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							Red/yellow Papaya
-						</td>
-						<td
-							style={{
-								color: '#485935',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontWeight: '400',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-								borderLeft: '1px solid #485935',
-								borderRight: '1px solid #485935',
-							}}
-						>
-							6
-						</td>
-						<td
-							style={{
-								color: '#A0D900',
-								fontSize: 20,
-								fontFamily: 'Poppins',
-								fontStyle: 'italic',
-								fontWeight: '900',
-								wordWrap: 'break-word',
-								textAlign: 'center',
-							}}
-						>
-							Near-low stock
-						</td>
-					</tr>
+					{products.map((product) => {
+						const totalQuantity = calculateTotalQuantity(
+							product.warehouse_products,
+							product.shelf_products,
+						)
+						const { status, color } = determineStatus(totalQuantity)
+						return (
+							<tr key={product.product_id}>
+								<td
+									style={{
+										padding: '8px',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									<img
+										src={defaultImageUrl}
+										alt={product.product_name}
+										style={{ height: '70px' }}
+									/>
+								</td>
+								<td
+									style={{
+										color: '#485935',
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontWeight: '400',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									{product.product_name}
+								</td>
+								<td
+									style={{
+										color: '#485935',
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontWeight: '400',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+										borderLeft: '1px solid #485935',
+										borderRight: '1px solid #485935',
+									}}
+								>
+									{totalQuantity}
+								</td>
+								<td
+									style={{
+										color: color,
+										fontSize: 20,
+										fontFamily: 'Poppins',
+										fontStyle: 'italic',
+										fontWeight: '900',
+										wordWrap: 'break-word',
+										textAlign: 'center',
+									}}
+								>
+									{status}
+								</td>
+							</tr>
+						)
+					})}
 				</tbody>
 			</table>
 		</div>
